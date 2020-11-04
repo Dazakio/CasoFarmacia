f097ca268ecde280e3cf3872e5966dd477245628
Daza <j.urra@alumnos.duoc.cl>
2020-11-04 09:58:18 -0300

    Agregué clase Afiliación y Clase Cliente

---
+++ CasoFarmacia/Models/Afiliacion.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace CasoFarmacia.Models
+{
+    public class Afiliacion
+    {
+        public string Codigo_salud { get; set; }
+        public string Nombre_salud { get; set; }
+        public double Descuento { get; set; }
+    }
+}
