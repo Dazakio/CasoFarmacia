0a9e090fedec6909bd1fa219dc9ccc07ac08bbee
08kidd <cristtob4lmills@gmail.com>
2020-11-11 09:45:26 -0300

    PRUEBA

--- CasoFarmacia/Models/Farmaceutico.cs
+++ CasoFarmacia/Models/Farmaceutico.cs
@@ -8,7 +8,7 @@ namespace CasoFarmacia.Models
     public class Farmaceutico
     {
         public string Rut_farmaceutico { get; set; }
-        public string Nombre_farmaceutico { get; set; }
+        public string Nombre_farmaceutico    { get; set; }
         public string Direccion_farmaceutico { get; set; }
         public string Email_farmaceutico { get; set; }
     }
